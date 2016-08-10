4cb81d0e3db86ca319bad8437a24bfe0281a88bd
Tarfand-pro <tarfand-pro021205555d55d@divismail.ru>
2016-08-10 20:01:01 +0430

    Update steady.sh

--- steady.sh
+++ steady.sh
@@ -1,66 +1,3 @@
-#!/bin/bash
-# =====================================================================================================
-# Copyright (C) steady.sh v1.2 2016 iicc (@iicc1)
-# =====================================================================================================
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-# this program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# =======================================================================================================
-# It depends on Tmux https://github.com/tmux/tmux which is BSD-licensed
-# and Screen https://www.gnu.org/software/screen GNU-licensed.
-# =======================================================================================================
-# This script is intended to control the state of a telegram-cli telegram bot running in background.
-# The idea is to get the bot fully operative all the time without any supervision by the user.
-# It should be able to recover the telegram bot in any case telegram-cli crashes, freezes or whatever.
-# This script works by tracing ctxt swithes value in kernel procces at a $RELOADTIME 
-# So it can detect any kind of kernel interruption with the procces and reload the bot.
-#
-#--------------------------------------------------
-#--    ▒▒▒▒▒▒▒▒▒                                --
-#--    ▒▒█████▒▒                                --
-#--    ▒█▒▒▒▒▒█▒                                --
-#--    ▒█▒▒▒▒▒█▒                                --
-#--    ▒█▒█▒▒▒█▒                                --
-#--    ▒▒██▒▒█▒▒                                --
-#--    ▒▒▒▒▒▒▒▒▒                                --
-#--    ▒███████▒                                --
-#--    ▒▒▒▒█▒▒█▒                                --
-#--    ▒▒▒▒█▒▒█▒                                --
-#--    ▒▒▒▒█▒▒█▒                                --
-#--    ▒▒▒▒▒██▒▒                                --
-#--    ▒▒▒▒▒▒▒▒▒                                --
-#--    ▒███████▒                                --
-#--    ▒▒▒▒▒▒█▒▒                                --
-#--    ▒▒▒▒██▒▒▒                                --
-#--    ▒▒▒▒▒▒█▒▒                                --
-#--    ▒███████▒                                --
-#--    ▒▒▒▒▒▒▒▒▒                                --
-#--    ▒▒█████▒▒                                --
-#--    ▒█▒▒▒▒▒█▒                                --
-#--    ▒█▒▒▒▒▒█▒                                --
-#--    ▒█▒▒▒▒▒█▒                                --
-#--    ▒▒█████▒▒                                --
-#--    ▒▒▒▒▒▒▒▒▒                                --
-#--    ▒███████▒                                --
-#--    ▒█▒▒▒▒▒█▒                                --
-#--    ▒█▒▒▒▒▒█▒                                --
-#--    ▒█▒▒▒▒▒█▒                                --
-#--    ▒▒█████▒▒                                --
-#--    ▒▒▒▒▒▒▒▒▒                                --                
-#--                                              --                   
-#--------------------------------------------------
-#--                                              --
-#--       Developers: @CRUEL                     --
-#--                                              --
-#--                                              --
-#--------------------------------------------------
-
-
 # Some script variables
 OK=0
 BAD=0
@@ -69,7 +6,7 @@ NONVOLUNTARYCHECK=0
 VOLUNTARY=1
 VOLUNTARYCHECK=0
 I=1
-BOT=Doostiha  # You can put here other bots. Also you can change it to run more than one bot in the same server.
+BOT=Tarfand  # You can put here other bots. Also you can change it to run more than one bot in the same server.
 RELOADTIME=10  # Time between checking cpu calls of the cli process. Set the value high if your bot does not receive lots of messages.
 
 
@@ -96,7 +33,7 @@ cat << EOF
  
 EOF
 echo -e "                \e[100m                Steady script           \e[00;37;40m"
-echo -e "               \e[01;34m                    by CRUEL M.KH                 \e[00;37;40m"
+echo -e "               \e[01;34m                    by getandroid                 \e[00;37;40m"
 echo ""
 cat << EOF
  $bld$f1▄ ▀▄   ▄▀ ▄   $f2 ▄▄▄████▄▄▄    $f3  ▄██▄     $f4▄ ▀▄   ▄▀ ▄   $f5 ▄▄▄████▄▄▄    $f6  ▄██▄  $rst
@@ -122,11 +59,11 @@ sleep 0.5
 
 
 echo ""
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
+echo -e "\033[38;5;208m @GPMod    :)      @getandroid \033[0;00m"
+echo -e "\033[38;5;208m @GPMod    :)      @getandroid \033[0;00m"
+echo -e "\033[38;5;208m @GPMod    :)      @getandroid \033[0;00m"
+echo -e "\033[38;5;208m @GPMod    :)      @getandroid \033[0;00m"
+echo -e "\033[38;5;208m @GPMod    :)      @getandroid \033[0;00m"
 
 sleep 1.5
 echo -e "$bld$f4 CHECKING PROCESSES...$rst"
@@ -274,11 +211,11 @@ sleep 0.5
 
 
 echo ""
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
+echo -e "\033[38;5;208m @GPMod    :)      @getandroid \033[0;00m"
+echo -e "\033[38;5;208m @GPMod    :)      @getandroid \033[0;00m"
+echo -e "\033[38;5;208m @GPMod    :)      @getandroid \033[0;00m"
+echo -e "\033[38;5;208m @GPMod    :)      @getandroid \033[0;00m"
+echo -e "\033[38;5;208m @GPMod    :)      @getandroid \033[0;00m"
 # Starting preliminar setup
 sleep 1.5
 echo -e "$bld$f4 CHECKING PROCESSES...$rst"
@@ -544,11 +481,11 @@ while getopts ":tsTSih" opt; do
 	echo ""
 	echo -e "\e[0m"
 
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
-echo -e "\033[38;5;208m @GPMod    :)      @cruel_channel \033[0;00m"
+echo -e "\033[38;5;208m @GPMod    :)      @getandroid \033[0;00m"
+echo -e "\033[38;5;208m @GPMod    :)      @getandroid \033[0;00m"
+echo -e "\033[38;5;208m @GPMod    :)      @getandroid \033[0;00m"
+echo -e "\033[38;5;208m @GPMod    :)      @getandroid \033[0;00m"
+echo -e "\033[38;5;208m @GPMod    :)      @getandroid \033[0;00m"
 echo ""
 	exit 1
       ;;
@@ -590,4 +527,3 @@ echo ""
       ;;
   esac
 done
-
