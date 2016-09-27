a46dcad3a87927d269e76988eed5850b79943f4c
Tarfand-pro <tarfand-pro021205555d55d@divismail.ru>
2016-09-27 18:06:53 +0330

    Add files via upload

--- plugins/help.lua
+++ plugins/help.lua
@@ -1,72 +1,110 @@
-function run(msg, matches)
-	
-    if is_realm(msg) then
-    if is_sudo(msg) or is_vip(msg) then
-     return "Realm hasn`t help\nRealm is a place for sudo users!"
-	end
-	end
-	
-if is_momod(msg) or is_owner(msg) or is_sudo(msg) or is_vip(msg) then
-  if matches[1] == "help" then
-  if msg.to.type == 'channel' and is_super_group(msg) then
-    one = io.open("./system/adv/team", "r")
-    two = io.open("./system/adv/channel", "r")
-    local team = one:read("*all")
-    local channel = two:read("*all")
-  HelpSuper = io.open("./helps/HelpSuper.txt", "r")
-  help_text_super = HelpSuper:read("*all")
-    send_msg("channel#id"..msg.to.id, help_text_super..'\nPowered by '..team..'\n<a href="'..channel..'">Join our channel :)</a>', ok_cb, false)
-  elseif msg.to.type == 'channel' and not is_super_group(msg) then
-    return "SupreGroup is not added!"
-  end
-	
-    if msg.to.type == 'chat' and is_group(msg) then
-    one = io.open("./system/adv/team", "r")
-    two = io.open("./system/adv/channel", "r")
-    local team = one:read("*all")
-    local channel = two:read("*all")
-	HelpChat = io.open("./helps/HelpChat.txt", "r")
-    	help_text = HelpChat:read("*all")
-    send_msg("chat#id"..msg.to.id, help_text..'\nPowered by '..team..'\n<a href="'..channel..'">Join our channel :)</a>', ok_cb, false)
-	 elseif msg.to.type == 'chat' and not is_group(msg) then
-	  return "Group is not added!"
-	end
-	end
-elseif not is_momod(msg) or not is_owner(msg) or not is_vip(msg) then
- if is_group(msg) or is_super_group(msg) then
-	  return "You cant see /help text"
- end
-end
-
- if msg.to.type == 'channel' and matches[1] == "sethelp" and matches[2] then
-    text = matches[2]
-    local b = 1
-    while b ~= 0 do
-    text = text:trim()
-    text,b = text:gsub('^!+','')
-  end
-  file = io.open("./helps/HelpSuper.txt", "w")
-  file:write(text)
-  file:flush()
-  file:close()
-  return "HelpSuper text has been changed to:\n"..matches[2]
- end
- if msg.to.type == 'chat' and matches[1] == "sethelp" and matches[2] then
-    text = matches[2]
-    local b = 1
-    while b ~= 0 do
-    text = text:trim()
-    text,b = text:gsub('^!+','')
-  end
-  file = io.open("./helps/HelpChat.txt", "w")
-  file:write(text)
-  file:flush()
-  file:close()
-  return "HelpChat text has been changed to:\n"..matches[2] 
- end
+do
+ function run(msg, matches)
+return [[
+مشخصات گروه و فرد!
+#info
+—------------------------------
+لیست ادمین های گروه!
+#admins
+—------------------------------
+بلاک کردن و کیک کردن فرد از گروه!
+#kick [reply|id] 
+—------------------------------
+لیست بات های موجود تو گروه!
+#bots
+—------------------------------
+بن کردن شخص خاطی!
+#ban [id|username|reply]
+—------------------------------
+خارج کردن از بن شخص مورد نظر!
+#unban [id|username]
+—------------------------------
+فیلتر کردن یک کلمه!
+#filter [word]
+—------------------------------
+در اوردن کلمه از فیلتر!
+#unfilter [word]
+—------------------------------
+نشان دادن کلمه های فیلتر شده!
+#filterlist
+—------------------------------
+نمایش آیدی خریدار گروه!
+#owner
+—------------------------------
+تنظیم یک متن به عنوان متن خوشامد گویی!
+#setwlc[Text]
+—------------------------------
+حذف پیام خوشامد گویی!
+#delwlc
+—------------------------------
+اطلاعت خود!
+#idme
+—------------------------------
+مقام دادن به دستار ادمینی!
+#promote [id|username|reply]
+—------------------------------
+برداشتن از مقام دستیار ادمینی!
+#demote [id|username]
+—------------------------------
+تنظیم نام گروه!
+#setname [namegp]
+—------------------------------
+تنظیم عکس گروه!
+#setphoto
+—------------------------------
+تنظیم قوانین گروه!
+#setrules
+—------------------------------
+دریافت قوانین گروه!
+#rules
+—------------------------------
+ساخت لینک جدید! درصورت ساخته گپ توسط بات!
+#newlink 
+—------------------------------
+تنظیم لینک درصورت ساخته نشدن گپ توسط بات!
+#setlink
+—------------------------------
+قفل کردن موارد زیر!
+#lock [links|flood|relpy|fwd|spam|english|join|username|tag|media|fosh|bots|all|operator|Arabic|member|rtl|sticker|contacts|strict|tgservice]
+—------------------------------
+ازاد کردن موارد قفل شده!
+#unlock [links|flood|relpy|fwd|spam|english|join|username|tag|media|fosh|bots|all|operator|Arabic|member|rtl|sticker|contacts|strict|tgservice]
+—------------------------------
+حالت سکوت موارد زیر!
+#mute [all|audio|gifs|photo|video|service]
+—------------------------------
+خارج کردن از حالت سکوت!
+#unmute [all|audio|gifs|photo|video|service]
+—------------------------------
+تنظیم حساسیت گروه!
+#setflood [5-20]
+—------------------------------
+دریافت لیست تنظیمات گروه!
+#settings
+—------------------------------
+حالت سکوت شخص مورد نظر!
+#silent [id|username|reply]
+ریمو از لیست موت شده ها!
+#unsilent [id|username|reply]
+—------------------------------
+پاک کردن موارد تنظیم شده!
+#clean [rules|about|modlist|mutelist]
+—------------------------------
+ریمو کردن تعداد چت تا مرز 100!
+#rm [2-100]
+—------------------------------
+خاموش و روشن کردن بات!
+#bot [on|off]
+—------------------------------
+⚠️میتوان از کاراکتر های [/!] به جای[#]استفاده کرد
+]]
 end
 return {
-advan = "http://github.com/janlou",
-patterns = {"^[!#/](help)$","^[!#/]([Ss]ethelp) (.*)$"},
-run = run,
+patterns = {
+"^[!/#][Hh]elp$",
+"^[Hh]elp$",
+"^❓$"
+},
+run = run
 }
+end
