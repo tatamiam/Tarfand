be6f56dd22d12bea3907d7ed8a5558bf464e93cf
telseed <telseedpower@gmail.com>
2016-12-22 12:06:44 +0330

    seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -267,11 +267,6 @@ function create_config( )
     about_text = [[Teleseed v4
 An advanced administration bot based on TG-CLI written in Lua
 
-https://github.com/Tarfand-pro/Tarfand
-
-Admins
-@NECCBOT
-
 ]],
     help_text_realm = [[
 Realm Commands:
